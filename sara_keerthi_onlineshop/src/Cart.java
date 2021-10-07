401f4ec2f0333a82891dd0f6b63738cc1fb53bff
Kvvrtv <kvvrtv@users.noreply.github.com>
2021-10-07 09:38:46 +0200

    1

--- sara_keerthi_onlineshop/src/Cart.java
+++ sara_keerthi_onlineshop/src/Cart.java
@@ -3,11 +3,16 @@ import com.sun.xml.internal.bind.v2.model.core.ID;
 public class Cart {
     private int IDcart;
     private Article arrayOfArticles[];
+    private Customer customer;
 
     public Cart(int IDcart, Article arrayOfArticles[]){
         this.IDcart = IDcart;
         this.arrayOfArticles = arrayOfArticles;
 
+    }
+
+    public Order createOrder(){
+        return null;
     }
 
 }
---
+++ sara_keerthi_onlineshop/src/CartTest.java
@@ -0,0 +1,8 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class CartTest {
+
+    @org.junit.jupiter.api.Test
+    void createOrder() {
+    }
+}
