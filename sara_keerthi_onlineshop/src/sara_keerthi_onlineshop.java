c18a8883fe9eecdb31b3aaa3a17c8ad7ff3e1dcc
Sara <Sara@LAPTOP-SARA>
2022-01-25 22:09:19 +0100

    db anbindung WIP

--- sara_keerthi_onlineshop/src/sara_keerthi_onlineshop.java
+++ sara_keerthi_onlineshop/src/sara_keerthi_onlineshop.java
@@ -1,34 +1,20 @@
+import database.BackendFactory;
+import database.BackendJdbcFactory;
 import models.Article;
 import models.Cart;
 import models.CheckoutController;
 import models.Customer;
 
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class sara_keerthi_onlineshop {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
 
-        Customer customer = new Customer(1, "test", "123", "test", "test",
-                "test", "sdff@dkfmdfm.ch");
+        BackendFactory backend = new BackendJdbcFactory();
 
-        Cart cart = new Cart(new ArrayList<>(), customer );
-
-        Article article = new Article("Hose", 9999, 2, "red", "test",
-                "test", 10);
-
-        Article article2 = new Article("Shirt", 9998, 2, "red", "test",
-                "test", 10);
-
-        cart.addItems(article, 2);
-        cart.addItems(article2, 3);
-
-        CheckoutController checkout = new CheckoutController();
-        String confirmation = checkout.CheckOut(cart);
-
-        System.out.println(confirmation);
-        
-        
+        backend.createUser("testuser", "test", "test", "test", "test", "test");
 
     }
 }
