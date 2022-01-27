2ef71dd0c12472c8f1025f5f2b707543af1191ee
Sara <Sara@LAPTOP-SARA>
2022-01-27 09:31:48 +0100

    db anbindung WIP

--- sara_keerthi_onlineshop/src/models/Customer.java
+++ sara_keerthi_onlineshop/src/models/Customer.java
@@ -1,52 +1,10 @@
 package models;
 
+import java.sql.SQLException;
 import java.util.Date;
 
 /** for customer registration */
-public class Customer{
+public abstract class Customer{
 
-    private User user;
-    private Date birthday;
-    private String phone;
-    private String gender;
-
-    public Customer(User user, Date birthday, String phone, String gender){
-        this.user = user;
-        this.birthday = birthday;
-        this.phone = phone;
-        this.gender = gender;
-    }
-
-    public void setBirthday(Date birthday){
-        this.birthday = birthday;
-    }
-
-    public Date getBirthday(){
-        return this.birthday;
-    }
-
-    public void setphone(String phone){
-        this.phone = phone;
-    }
-
-    public String getphone(){
-        return this.phone;
-    }
-
-
-    public void setgender(String gender){
-        this.gender = gender;
-    }
-
-    public String getgender(){
-        return this.gender;
-    }
-
-    public void setUser(User user) {
-        this.user = user;
-    }
-
-    public User getUser() {
-        return user;
-    }
+    public abstract void delete() throws SQLException;
 }
---
+++ sara_keerthi_onlineshop/src/models/Customer_JdbcImpl.java
@@ -0,0 +1,9 @@
+package models;
+
+public class Customer_JdbcImpl {
+    private final int IDuser;
+
+    public Customer_JdbcImpl(String vorname, String nachname, String adresse, String email, String username, String passwort){
+
+    }
+}
