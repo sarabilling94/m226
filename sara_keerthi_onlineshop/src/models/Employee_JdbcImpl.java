9121e85da2ed724e4b3c5dbffa011eb95657f45e
Sara <Sara@LAPTOP-SARA>
2022-02-02 16:32:41 +0100

    added getByID functions in user, employee, customer

--- sara_keerthi_onlineshop/src/models/Employee_JdbcImpl.java
+++ sara_keerthi_onlineshop/src/models/Employee_JdbcImpl.java
@@ -22,12 +22,13 @@ public class Employee_JdbcImpl extends Employee{
         User_JdbcImpl person = new User_JdbcImpl(vorname, nachname, adresse, email, username, passwort);
         this.IDuser = person.getIDuser();
         Connection conn = JdbcDb.getConnection();
-        PreparedStatement employee_stmt = conn.prepareStatement("insert into tbl_mitarbeiter (FS_person,geburtstag,geschlecht,telefon,admin) values(?,?,?,?,?)");
+        PreparedStatement employee_stmt = conn.prepareStatement("insert into tbl_mitarbeiter (ID_mitarbeiter, FS_person,geburtstag,geschlecht,telefon,admin) values(?,?,?,?,?,?)");
         employee_stmt.setInt(1, IDuser);
-        employee_stmt.setDate(2, new java.sql.Date(birthday.getTime()));
-        employee_stmt.setString(3, geschlecht);
-        employee_stmt.setString(4, telefon);
-        employee_stmt.setBoolean(5, admin);
+        employee_stmt.setInt(2, IDuser);
+        employee_stmt.setDate(3, new java.sql.Date(birthday.getTime()));
+        employee_stmt.setString(4, geschlecht);
+        employee_stmt.setString(5, telefon);
+        employee_stmt.setBoolean(6, admin);
         employee_stmt.executeUpdate();
     }
 
@@ -37,6 +38,35 @@ public class Employee_JdbcImpl extends Employee{
 
     public int getIDuser(){
         return IDuser;
+    }
+
+    public static Employee_InMemoryImpl getEmployeeById(int IDuser){
+        try {
+            Connection conn = JdbcDb.getConnection();
+            PreparedStatement employee_stmt = conn.prepareStatement("select * from tbl_mitarbeiter where FS_person=?");
+            employee_stmt.setInt(1, IDuser);
+            employee_stmt.execute();
+            ResultSet res = employee_stmt.getResultSet();
+            res.next();
+            Date birthday = res.getDate("geburtstag");
+            String gender = res.getString("geschlecht");
+            String phone = res.getString("telefon");
+            boolean admin = res.getBoolean("admin");
+
+            PreparedStatement user_stmt = conn.prepareStatement("select * from tbl_person where ID_person=?");
+            user_stmt.setInt(1, IDuser);
+            user_stmt.execute();
+            ResultSet res2 = user_stmt.getResultSet();
+            res2.next();
+
+            Employee_InMemoryImpl employee = new Employee_InMemoryImpl(IDuser, res2.getString("vorname"), res2.getString("nachname"),
+                    res2.getString("adresse"), res2.getString("email"), res2.getString("username"),res2.getString("passwort"),
+                    birthday, phone, gender, admin);
+            return employee;
+        } catch (SQLException | ImpossibleBirthdateException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
     @Override
